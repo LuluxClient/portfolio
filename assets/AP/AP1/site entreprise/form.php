c0c2ab4ced1cc0735b8ab1fe334162a17adaa0f1
Luca Améaume <62576432+LuluxClient@users.noreply.github.com>
2022-11-29 15:28:24 +0100

    Ajout d'une 2ème page AP1 et ajout des documents.

---
+++ assets/AP/AP1/site entreprise/form.php
@@ -0,0 +1,10 @@
+<?php
+    $host = "localhost";
+    $port = "3307";
+    $dbname = "form";
+    $username = "root";
+    $password = "";
+
+    try {
+        $conn new PDO('mysql.hosts'.$host';port=3037')
+    }
